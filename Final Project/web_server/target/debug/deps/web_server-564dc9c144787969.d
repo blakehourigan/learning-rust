242b6d54d5197ce9657b2521127847a49839876f
blake <Houriganblake@proton.me>
2025-01-05 11:22:13 -0500

    ignore file

--- Final Project/web_server/target/debug/deps/web_server-564dc9c144787969.d
+++ Final Project/web_server/target/debug/deps/web_server-564dc9c144787969.d
@@ -1,5 +1,5 @@
-/home/blake/Documents/rust/Final Project/web_server/target/debug/deps/libweb_server-564dc9c144787969.rmeta: src/lib.rs
+/home/blake/Documents/learning-rust/Final Project/web_server/target/debug/deps/libweb_server-564dc9c144787969.rmeta: src/lib.rs
 
-/home/blake/Documents/rust/Final Project/web_server/target/debug/deps/web_server-564dc9c144787969.d: src/lib.rs
+/home/blake/Documents/learning-rust/Final Project/web_server/target/debug/deps/web_server-564dc9c144787969.d: src/lib.rs
 
 src/lib.rs:
--- Final Project/web_server/target/debug/deps/web_server-93ecc1b39cf3cf75.d
+++ Final Project/web_server/target/debug/deps/web_server-93ecc1b39cf3cf75.d
@@ -1,5 +1,5 @@
-/home/blake/Documents/rust/Final Project/web_server/target/debug/deps/libweb_server-93ecc1b39cf3cf75.rmeta: src/main.rs
+/home/blake/Documents/learning-rust/Final Project/web_server/target/debug/deps/libweb_server-93ecc1b39cf3cf75.rmeta: src/main.rs
 
-/home/blake/Documents/rust/Final Project/web_server/target/debug/deps/web_server-93ecc1b39cf3cf75.d: src/main.rs
+/home/blake/Documents/learning-rust/Final Project/web_server/target/debug/deps/web_server-93ecc1b39cf3cf75.d: src/main.rs
 
 src/main.rs:
